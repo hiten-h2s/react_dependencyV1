dbfc4f785f34a7045e20bc5b6e25f362d7ace735
Mayank-kush24 <mayank.kush@hack2skill.com>
2026-01-21 17:12:58 +0530

    Initial commit: React + Vite project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to React</h1>
+      <p>Your app is ready to go!</p>
+    </div>
+  )
+}
+
+export default App
+
